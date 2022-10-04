90e6324ccd7c7fc2fa7f5fe10d061fabf73d83f2
dongwoo.kang <dongwoo.kang@appg.co.kr>
2022-10-04 11:28:22 +0900

    Initial commit

---
+++ PDFKitTutorialUITests/PDFKitTutorialUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PDFKitTutorialUITestsLaunchTests.swift
+//  PDFKitTutorialUITests
+//
+//  Created by 앱지 Appg on 2022/10/04.
+//
+
+import XCTest
+
+final class PDFKitTutorialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
