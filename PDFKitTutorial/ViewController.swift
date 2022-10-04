d9ee6408003642230e65ba31fb1933a64bd29834
dongwoo.kang <ehddn3821@naver.com>
2022-10-04 14:13:37 +0900

    Add tappedGeneratePDF

--- PDFKitTutorial/ViewController.swift
+++ PDFKitTutorial/ViewController.swift
@@ -6,17 +6,95 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var queryTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
     @IBAction func tappedGeneratePDF(_ sender: Any) {
+        NetworkManager.shared.getCocktails(searchQuery: queryTextField.text ?? "") { menu in
+            DispatchQueue.main.async {
+                let pdfData = self.generatePDFData(drinks: menu.drinks ?? [])
+                let pdfView = PDFView()
+            }
+        } failure: { error in
+            print(error)
+        }
+    }
+    
+    func generateDrinkText(drink: Cocktail, context: UIGraphicsPDFRendererContext, cursorY: CGFloat, pdfSize: CGSize) -> CGFloat {
+        var cursor = cursorY
+        let leftMargin: CGFloat = 74
         
+        if let drinkName = drink.strDrink {
+            cursor = context.addSingleLineText(fontSize: 14, weight: .bold, text:  drinkName, indent: leftMargin, cursor: cursor, pdfSize: pdfSize, annotation: .underline, annotationColor: .black)
+            cursor+=6
+            
+            cursor = context.addSingleLineText(fontSize: 12, weight: .thin, text: "\(drink.strCategory ?? "Cocktail") | \(drink.strAlcoholic ?? "Might Contain Alcohol")", indent: leftMargin, cursor: cursor, pdfSize: pdfSize, annotation: nil, annotationColor: nil)
+            cursor+=2
+            
+            cursor = addDrinkIngredients(drink: drink, context: context, cursorY: cursor, pdfSize: pdfSize)
+
+            if let instructions = drink.strInstructions {
+                cursor = context.addMultiLineText(fontSize: 9, weight: .regular, text: instructions, indent: leftMargin, cursor: cursor, pdfSize: pdfSize)
+                cursor+=8
+            }
+            
+            cursor+=8
+        }
+        return cursor
+    }
+
+    func addDrinkIngredients(drink: Cocktail, context: UIGraphicsPDFRendererContext, cursorY: CGFloat, pdfSize: CGSize) -> CGFloat {
+        let ingredients = [drink.strIngredient1, drink.strIngredient2, drink.strIngredient3, drink.strIngredient4]
+        let measures = [drink.strMeasure1, drink.strMeasure2, drink.strMeasure3, drink.strMeasure4]
+        var cursor = cursorY
+        let leftMargin: CGFloat = 74
+        
+        for i in 0..<ingredients.count {
+            if let ingredient = ingredients[i], let measure = measures[i] {
+                cursor = context.addSingleLineText(fontSize: 12, weight: .thin, text: " • \(ingredient) (\(measure))", indent: leftMargin, cursor: cursor, pdfSize: pdfSize, annotation: nil, annotationColor: nil)
+                cursor+=2
+            }
+        }
+        
+        cursor+=8
+        
+        return cursor
+    }
+    
+    func generatePDFData(drinks: [Cocktail]) -> Data {
+        let pdfMetaData = [
+            kCGPDFContextCreator: "thecocktaildb",
+            kCGPDFContextAuthor: "dwKang",
+            kCGPDFContextTitle: "Cocktails Menu"
+        ]
+        
+        let format = UIGraphicsPDFRendererFormat()
+        format.documentInfo = pdfMetaData as [String: Any]
+        
+        let pageRect = CGRect(x: 10, y: 10, width: 595.2, height: 841.8)  // Page size is set A4
+        let graphicsRenderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
+        
+        let data = graphicsRenderer.pdfData { context in
+            context.beginPage()
+            
+            let initialCursor: CGFloat = 32
+            
+            var cursor = context.addCenteredText(fontSize: 32, weight: .bold, text: "Cocktail Menu 🍹", cursor: initialCursor, pdfSize: pageRect.size)
+            
+            cursor += 42  // Add white space after the Title
+            
+            for drink in drinks {
+                cursor = generateDrinkText(drink: drink, context: context, cursorY: cursor, pdfSize: pageRect.size)
+            }
+        }
+        return data
     }
 }
 
